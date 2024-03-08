e412608600e2c33b8e94f5bac2d14c056fca4ca1
Zypher <54479617+Rehpyz@users.noreply.github.com>
2024-03-08 13:33:14 +0000

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -31,8 +31,8 @@ module.exports = {
             .setColor('#da2a41')
             .setAuthor({
           name: 'Owner',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://tenor.com/view/roblox-roblox-meme-roblox-memes-boi-wtf-twerk-gif-6690897771582534374',
+          url: 'https://discord.gg/rSTxtq2NHc'
         })
             .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
             .setTimestamp();
